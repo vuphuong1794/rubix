5a5e5e8f5e2289d3783f470b5727fb8db72c9f9a
hieuhoi91 <hieuhoi912002@gmail.com>
2023-05-10 00:32:32 +0700

    change domain

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,7 @@
 export const ERROR_TOKEN = 'RefreshAccessTokenError';
 
-export const BASE_URL_API = 'https://materialize-production.up.railway.app';
+export const BASE_URL_API =
+  'https://materialize-production-df07.up.railway.app';
 
 export const ROUTES = {
   LOGIN: '/login',
