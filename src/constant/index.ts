ad4ebd6561260d02d810d689b84e5ff83be14110
Tran Ngoc Thanh <97892253+thanhbk113@users.noreply.github.com>
2023-06-27 08:39:31 +0700

    change url

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,7 +1,7 @@
 export const ERROR_TOKEN = 'RefreshAccessTokenError';
 
 export const BASE_URL_API =
-  'https://materialize-production-df07.up.railway.app';
+  'https://tmdt.up.railway.app';
 
 export const ROUTES = {
   LOGIN: '/login',
