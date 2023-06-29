fd0b15afe4e2459a982ade55d21f08195d7cea68
thanhbk113 <thanhbk11a1@gmail.com>
2023-06-29 16:25:54 +0700

    remove unused

--- src/components/overlay/Potal.tsx
+++ src/components/overlay/Potal.tsx
@@ -1,20 +1,9 @@
 import ReactDOM from 'react-dom';
 
-import { useAppDispatch } from '@/app/hooks';
-import { closeShoppingCart } from '@/features/cart/cartSlice';
-
 // TODO: Need refactoring
 const Potal = () => {
-  const dispatch = useAppDispatch();
-  const handleCloseShoppingCart = () => {
-    dispatch(closeShoppingCart());
-  };
-
   return ReactDOM.createPortal(
-    <div
-      className='fixed top-0 left-0 z-20 h-full w-full bg-black opacity-80'
-      onClick={handleCloseShoppingCart}
-    ></div>,
+    <div className='fixed top-0 left-0 z-20 h-full w-full bg-black opacity-80'></div>,
     document.querySelector('body') as HTMLElement
   );
 };
