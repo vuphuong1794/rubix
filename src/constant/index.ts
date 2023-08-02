d8e925d3a078e6ed60809a2cd85b087a1282303f
thanhbk113 <thanhbk113@gmail.com>
2023-08-02 08:58:58 +0700

    change api

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,6 @@
 export const ERROR_TOKEN = 'RefreshAccessTokenError';
 
-export const BASE_URL_API = 'https://tmdt-api-7i6b.onrender.com/';
+export const BASE_URL_API = 'https://thanhbk113.click/';
 
 export const ROUTES = {
   LOGIN: '/login',
